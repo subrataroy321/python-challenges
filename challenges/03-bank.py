f6cf47fbed15377f2e9f27c11dc47e9f7583ca8d
subrataroy321 <subrata.r321@gmail.com>
2020-09-14 16:32:03 -0400

    add solutions for challanges

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,38 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+balance = 5000
+
+def deposit(balance):
+    amount = input("Enter the amount you want to deposit: ")
+    amount = int(amount)
+    balance = balance + amount
+    print(f"Your current balance is {balance}")
+    finish(balance)
+
+def withdraw(balance):
+    amount = input("Enter the amount you want to withdraw: ")
+    amount = int(amount)
+    balance = balance - amount
+    print(f"Your current balance is {balance}")
+    finish(balance)
+
+def finish(balance):
+    finished = input("Are you finish? (yes or no)")
+    if(finished == "yes"):
+        print("Have a nice day!")
+    elif(finished == "no"):
+        doTransaction(balance)
+    
+def doTransaction(balance):
+    print(f"Your current balance is {balance}")
+    choice = input("What would you like to do? (deposit, withdraw, balance)")
+
+    if(choice == "deposit"):
+        deposit(balance)
+    elif(choice == "withdraw"):
+        withdraw(balance)
+    elif(choice == "balance"):
+        print(f"Your current balance is {balance}")
+
+doTransaction(balance)
 
